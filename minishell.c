2abcdc112642d3001b6992e4de91210e8a0fffce
alvachon <alvachon93@pm.me>
2023-01-31 19:42:11 -0500

    planification

--- minishell.c
+++ minishell.c
@@ -11,7 +11,9 @@
 /* ************************************************************************** */
 
 //gcc minishell.c -lreadline
-
+/*
+ * [1]BUILT [2]OPTION [3]REDIRECTION [4]APPEND [5]INFILE [6]PIPE [7]REDIRECTION [8]APPEND [9]OUTFILE
+ * */
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -23,16 +25,14 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-  i = 0;
-  while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) {
-    i++;
-  }
-  return (s1[i] - s2[i]);
+    int i;
+    i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) 
+        i++;
+    return (s1[i] - s2[i]);
 }
 
-int main(int ac, char **av, char **envp)
+int main(int ac, char **av, char **env)
 {
 	int		infile;
 	char	*cmd;
@@ -41,7 +41,7 @@ int main(int ac, char **av, char **envp)
 	{
   		printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
 		//signal redirection here
-  		while (1)//looper
+  		while (1)
 		{
     		cmd = readline("$ ");
     		if (!cmd)
@@ -49,17 +49,19 @@ int main(int ac, char **av, char **envp)
 				fprintf(stderr, "Usage: readline space allocation\n");
   				exit(EXIT_FAILURE);
 			}
-    		if (cmd[0] == '\0' || ft_strcmp(cmd, "\n") == 0) {
-      		printf("Nothing, command freed, continue.\n");
-      		free(cmd);
-      		continue;
+    		if (cmd[0] == '\0' || ft_strcmp(cmd, "\n") == 0)
+            {
+      	    	printf("Nothing, command freed, continue.\n");
+      		    free(cmd);
+      		    continue;
     		}
-    		if (ft_strcmp(cmd, "exit") == 0) {
-     		printf("exit, command freed, goodbye.\n");
-      		free(cmd);
-      		exit(EXIT_SUCCESS);
+    		if (ft_strcmp(cmd, "exit") == 0)
+            {
+     		    printf("exit, command freed, goodbye.\n");
+      		    free(cmd);
+      		    exit(EXIT_SUCCESS);
     		}
-			//lexical_parsing_here(cmd);, ne pas oublier de free si caca.
+			//lexical_parsing_here(cmd);
     		printf("%s\n", cmd);
 			add_history(cmd);
 			printf("Command done and freed, added to the history\n");
