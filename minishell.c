8ce29340931e0c19857a7528c9220501a3753100
Alexandra Vachon <alvachon@c3r5p5.42quebec.com>
2023-02-06 12:58:47 -0500

    init_shell_and_tests

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 13:27:55 by alvachon          #+#    #+#             */
-/*   Updated: 2023/02/01 12:14:20 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/02/06 12:57:34 by alvachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,32 @@
 #include<readline/readline.h>
 #include<readline/history.h>
 #include<errno.h>
+#include<termios.h>
+
+typedef struct s_process
+{
+	struct s_process	*next;//Next process in pipeline
+	char				*argv;//For exect
+	pid_t				pid;//Process ID
+	char				completed;//True if process has completed
+	char				stopped;//True if process has stopped
+	int					status;//report status value
+}	t_process
+;
+
+typedef struct s_job
+{
+	struct s_job	*next;//Next active job
+	char			*command;//Command line, used for messages
+	t_process		*first_process;//list of processes in this job
+	pid_t			pgid;//process group ID
+	char			notified;//true if user told about stopped job
+	struct termios	tmodes;//saved terminal modes
+	int				stdin;//standard entry (0) i/o channel
+	int				stdout;//standard exit (1) i/o channel
+	int				stderr;//standard error exit (2) i/o channel
+}	t_job
+;
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -33,18 +59,47 @@ int	ft_strcmp(char *s1, char *s2)
     return (s1[i] - s2[i]);
 }
 
+void	init_shell()
+{
+	int				shell_terminal;
+	int				shell_is_interactive;
+	struct termios	term1;
+
+
+	shell_terminal = STDIN_FILENO;
+	shell_is_interactive = isatty(shell_terminal);
+	if (shell_is_interactive)
+	{
+		if (tcgetattr(STDIN_FILENO, &term1) != 0)
+    		perror("tcgetattr() error");
+		else
+		{
+			printf("the original end-of-file character is x'%02x'\n", term1.c_cc[VEOF]);
+			term1.c_cc[VEOF] = 'z';
+ 			if (tcsetattr(STDIN_FILENO, TCSANOW, &term1) != 0)
+      			perror("tcsetattr() error");
+    		if (tcgetattr(STDIN_FILENO, &term1) != 0)
+      			perror("tcgetattr() error");
+   			else
+      			printf("the new end-of-file character is x'%02x'\n", term1.c_cc[VEOF]);
+		}
+	}
+}
+
 int main(int ac, char **av, char **env)
 {
-	int		infile;
+	//pid_t			shell_pgid;
 	char	*cmd;
 
 	if (ac != 2)
 	{
 		//THIS IS 42ALMINISHELL BRANCH * * * * * * * * * * * * * * * * * * 
   		printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
-		//signal redirection here
-  		while (1)
+		//Break Terminal Signals here
+		init_shell();
+  		while (1)//This part is named Foreground Job (gets input)
 		{
+			/*signal_handler(process_a);*/
     		cmd = readline("minishell$ ");
     		if (!cmd)
       		{
@@ -57,14 +112,22 @@ int main(int ac, char **av, char **env)
       		    free(cmd);
       		    continue;
     		}
-			//alloooooooo
     		if (ft_strcmp(cmd, "exit") == 0)
             {
      		    printf("exit, command freed, goodbye.\n");
       		    free(cmd);
       		    exit(EXIT_SUCCESS);
     		}
-			//lexical_parsing_here(cmd);
+			/*lexical_parsing_here(cmd);*/
+			//This part is Background job
+			/*signal_handler(process_b);*/
+			/*if (simple_command)
+				Do(command_builtins);*/
+			/*else
+			 {
+				fork_process();
+				-> in child (exect requested command and never return);
+				->* in parent (wait for child to complete); */
     		printf("%s\n", cmd);
 			add_history(cmd);
 			printf("Command done and freed, added to the history\n");
